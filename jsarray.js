031d7635aa1106ef3ceeff5541301895fc780523
Josias Castellanos <karikachomon@gmail.com>
2022-08-24 21:52:05 -0600

    Add files via upload
    Asi logre hacer los array

---
+++ jsarray.js
@@ -0,0 +1,11 @@
+//array's
+
+let array = [9,201,5,11,8];
+
+console.log(array)
+
+function arrayes(){
+    return "|" + "\n" +"|" + "\n" +"Este es el primer numero de tu array: " + (array[0]) +"\n" + "Este es el segundo numero de tu array: " + (array[1]) +"\n" +"Este es el tercer numero de tu array: " + (array[2]) +"\n" +"Este es el cuarto numero de tu array: " + (array[3]) +"\n" +"Este es el quinto numero de tu array: " + (array[4]);
+}
+
+console.log(arrayes());
